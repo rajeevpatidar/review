ab5ef58c974bc50399fd100a258325f0489b6fd3
Rajeev <rajeev2001.24@gmail.com>
2022-08-08 19:19:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState } from "react";
+import reviews from "./data";
+import "./App.css"
+// import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+// import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Button from '@mui/material/Button';
+import { useEffect } from "react";
+const App=()=>{
+    const [people,setPeople] = useState(reviews);
+    const [index,setIndex] = useState(0);
+    const {image,name,job,text} = people[index];
+    const goPrev=()=>{
+        if(index===0){
+            setIndex(()=>{
+                return reviews.length-1;
+            })
+        }
+        else{
+
+            setIndex((prev)=>{
+                return prev-1;
+            })
+        }
+    }
+    const goFor=()=>{
+        if(index===reviews.length-1){
+            setIndex(()=>{
+                return 0;
+            })
+        }
+        else{
+            setIndex((prev)=>{
+                return prev+1;
+            })
+        }
+    }
+    return(
+        <div className="container">
+            <div className="img-box">
+                <button className="btn" onClick={goPrev}> &lt;</button>
+                <img className="img-review" src={image} alt="" />
+                <button className="btn" onClick={goFor}>&gt;</button>
+            </div>
+            <div>
+                <h1>
+                {name}
+                </h1>
+            </div>
+            <div className="job">
+                <p>
+                {job}
+                </p>
+            </div>
+            <div className="text">
+                <p>{text}</p>
+            </div>
+            
+        </div>
+    )
+}
+export default App
